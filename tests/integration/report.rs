49467a00cda0f8bd3817037d1ea72035f122f90c
Christopher Morton <sonro@gmx.com>
2022-09-30 18:34:49 +0100

    ci: build binaries used in tests

--- tests/integration/report.rs
+++ tests/integration/report.rs
@@ -1,10 +1,10 @@
 use std::process::{Command, Output, Stdio};
 
-const STATUS_TEST_BIN: &str = "./target/debug/status_test";
+const STATUS_TEST_NAME: &str = "status_test";
 
 #[test]
 fn status_output_to_stderr() {
-    let output = test_bin(STATUS_TEST_BIN, &["hi", "world", "green"]);
+    let output = test_bin(STATUS_TEST_NAME, &["hi", "world", "green"]);
     // don't test for color because `status()` only outputs it to a TTY
     // by piping stderr in `test_bin` we therefore remove the color output
     let expected = format!("{:>12} {}\n", "hi", "world");
@@ -13,8 +13,11 @@ fn status_output_to_stderr() {
 }
 
 fn test_bin(binary: &str, args: &[&str]) -> Output {
-    Command::new(binary)
-        .args(args)
+    let mut full_args = vec!["run", "-q", "--bin", binary, "--"];
+    full_args.extend_from_slice(args);
+
+    Command::new("cargo")
+        .args(full_args)
         .stderr(Stdio::piped())
         .spawn()
         .unwrap_or_else(|err| panic!("Failed to execute binary for testing: {}. {}", binary, err))
